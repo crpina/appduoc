137eb641361c5f0e952b3950c7f9c42f63f4a92b
Esteban Tamayo Chamorro <es.tamayo@duocuc.cl>
2023-11-19 21:15:45 -0300

    Cambios en recuperar contraseña y Firma de app
    Se hace funcionar el enviar correo electronico al pulsar boton de recuperar contraseña y se realiza la firma solicitada por la rubrica

--- src/app/rcontrasena/rcontrasena.module.ts
+++ src/app/rcontrasena/rcontrasena.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,7 +13,8 @@ import { RcontrasenaPage } from './rcontrasena.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    RcontrasenaPageRoutingModule
+    RcontrasenaPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [RcontrasenaPage]
 })
--- src/app/rcontrasena/rcontrasena.page.ts
+++ src/app/rcontrasena/rcontrasena.page.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import emailjs from "@emailjs/browser";
 
 @Component({
   selector: 'app-rcontrasena',
   templateUrl: './rcontrasena.page.html',
   styleUrls: ['./rcontrasena.page.scss'],
 })
-export class RcontrasenaPage implements OnInit {
+export class RcontrasenaPage{
 
-  constructor() { }
+  form: FormGroup = this.fb.group({
+    from_email: '',
+    subject: '',
+    message: '',
+  });
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
   }
+
+  async send($event: MouseEvent) {
+    $event.preventDefault()
+    emailjs.init('QpyMnAHf5F5orCEN6');
+    let response  = await emailjs.send("service_hc0p41c","template_l2t4fdv",{
+      from_email: this.form.value.from_email,
+      subject: "Su contraseña es:",
+      message: "321",
+    });
+
+    alert('El mensaje a sido enviado.');
+    this.form.reset();
+  }
+
+
+
 
 }
